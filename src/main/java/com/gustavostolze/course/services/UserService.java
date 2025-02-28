db1dbd65b2abfa253c527151391d3376b9515112
Gustavo <gustavostolzedeandrade@gmail.com>
2025-02-28 11:01:24 -0300

    Service layer, component registration

---
+++ src/main/java/com/gustavostolze/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.gustavostolze.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavostolze.course.entities.User;
+import com.gustavostolze.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
