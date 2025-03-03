2b05bb2532259e9851a49dfdb9707104fcef126f
Gustavo Stolze <gustavostolzedeandrade@gmail.com>
2025-03-03 12:43:00 -0300

    User insert

--- src/main/java/com/gustavostolze/course/resources/UserResource.java
+++ src/main/java/com/gustavostolze/course/resources/UserResource.java
@@ -1,33 +1,44 @@
 package com.gustavostolze.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.gustavostolze.course.entities.Order;
-import com.gustavostolze.course.services.OrderService;
+import com.gustavostolze.course.entities.User;
+import com.gustavostolze.course.services.UserService;
 
 @RestController
-@RequestMapping(value="/orders")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
-	private OrderService orderService;
-	
+	private UserService userService;
+
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll() {
-		List<Order> list = orderService.findAll();
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id) {
-		Order obj = orderService.findById(id);
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
